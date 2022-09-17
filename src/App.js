e694f2ceea0448b35b8da7742ca67e984a80d678
ArtemRozzhivin <temchik200352@gmail.com>
2022-09-17 17:21:44 +0300

    guestList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [isInvated, setInvated] = useState([]);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => {
+        console.log(err);
+        alert('Ошибка в получение пользователей!');
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const changeSetSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const changeSetInvated = (id) => {
+    if (isInvated.includes(id)) {
+      setInvated((prev) => prev.filter((invateId) => invateId !== id));
+    } else {
+      setInvated((prev) => [...prev, id]);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {success ? (
+        <Success count={isInvated.length} />
+      ) : (
+        <Users
+          setSuccess={setSuccess}
+          isInvated={isInvated}
+          changeSetInvated={changeSetInvated}
+          search={search}
+          changeSetSearch={changeSetSearch}
+          items={users}
+          isLoading={isLoading}
+        />
+      )}
     </div>
   );
 }
